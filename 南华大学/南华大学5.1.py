b608f6e25bfc01ae0d8c1762355a54b02deaad53
tsuish-MacBook <13781255018@163.com>
2026-04-23 00:47:03 +0800

    add 南华大学5.1.py

---
+++ 南华大学/南华大学5.1.py
@@ -0,0 +1,24 @@
+"""
+@File: 南华大学5.1.py
+@Author: cuishuohao
+@Date: 2026/4/23 00:29
+
+https://article.xuexi.cn/articles/video/index.html?art_id=13235609235057512304&read_id=5f795b70-6c97-4614-85db-9e354f9f02fc&ref_read_id=1a62a21e-0c8b-423f-88e3-d28bc31ed6f7&reco_id=&mod_id=&cid=&source=&study_style_id=undefined
+5.1 程序的基本结构
+"""
+
+# 关系运算
+def func_1():
+    print(not 0)
+    print(not 'AA')
+    print('AA' and True)
+    print('' or False)
+    print(3+(not False))
+
+# in运算符
+def func_2():
+    print(5 in (20,15,3,5))
+    print(5 not in (20,15,3,5))
+
+# func_1()
+func_2()
